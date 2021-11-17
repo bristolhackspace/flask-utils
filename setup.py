2eacbceb4de9f79913f56a9b0701221192645383
Sam <633856+SamP20@users.noreply.github.com>
2021-11-17 19:55:26 +0000

    Template files not zip safe

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='bristolhackspace.flask_utils',
     packages=find_namespace_packages(include=['bristolhackspace.*']),
-    package_data={
-        "bristolhackspace": ["*"],
-    },
+    include_package_data=True,
+    zip_safe=False,
     install_requires=[
         'flask>=2.0',
         'yarl',
